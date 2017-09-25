f3af8b15196edb29dbd9d38329c626a13c95d784
RomainIEJ2016 <romain@rencontres-audiovisuelles.org>
2017-09-25 15:42:52 +0200

    GameObjects and MAterials for first prototype

---
+++ IEJ_Session6/Assets/Scripts/Orbs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbs : MonoBehaviour {
+
+	void Start () {
+
+        Invoke("Destruction", 3f);
+	}
+	
+	void Update () {
+		
+	}
+
+    void Destruction()
+    {
+
+        Destroy(gameObject);
+    }
+}
---
+++ IEJ_Session6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+
+    public GameObject orb;
+    private GameObject player;
+    private GameObject orbs;
+
+	void Start () {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        orbs = GameObject.Find("Orbs");
+
+    }
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LaunchOrb();
+        }
+
+	}
+
+    void LaunchOrb()
+    {
+        GameObject myOrb;
+        myOrb = Instantiate(orb, player.transform.position, Quaternion.identity, orbs.transform);
+        myOrb.GetComponent<Rigidbody>().AddForce(player.transform.position + player.GetComponentInChildren<Transform>().transform.forward *500);
+
+    }
+}

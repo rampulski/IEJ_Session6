48de5109cec78f0f56a6b294b9e9e37ea2987edb
IEJ-PC07\Game_03 <lecl.gg@gmail.com>
2017-09-26 11:03:19 +0200

    Modification Script Player (ajout lancer orb suivant mouvement camera).

---
+++ IEJ_Session6/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public GameObject orb;
+    private GameObject player;
+    private GameObject orbs;
+    public Camera KameraMain;
+    public Vector3 Kamera;
+    public int launchForce;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        orbs = GameObject.Find("Orbs");
+        launchForce = 800;
+        KameraMain = Camera.main;
+    }
+
+    void Update()
+    {
+        Kamera = KameraMain.transform.forward;
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            LaunchOrb();
+        }
+    }
+
+    void LaunchOrb()
+    {
+        GameObject myOrb;
+        myOrb = Instantiate(orb, player.transform.position + player.transform.forward, Quaternion.identity, orbs.transform);
+        myOrb.GetComponent<Rigidbody>().AddForce(Kamera * launchForce);
+    }
+}

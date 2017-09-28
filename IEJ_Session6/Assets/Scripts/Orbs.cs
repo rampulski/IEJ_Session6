73bea89d46b882faba2dbc613b41986a6cb89e28
Lenovo-PC\Sélom <akiel.arcosian@gmail.com>
2017-09-28 15:30:13 +0200

    no message

--- IEJ_Session6/Assets/Scripts/Orbs.cs
+++ IEJ_Session6/Assets/Scripts/Orbs.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class Orbs : MonoBehaviour {
 
-    public float timeBeforeDestruction;
-	void Start () {
 
-        Invoke("Destruction", timeBeforeDestruction);
-	}
-	
-	void Update () {
+	public GameObject Sphere;
+	public Light sphereLight;
+
+    public float timeBeforeDestruction;
+
+	void Start () {
 		
+
+		sphereLight = GetComponent<Light> ();
+
+
+
+	}
+	IEnumerator dontKillNow(){
+
+		yield return new WaitForSeconds (10);
+
+
+
+	}
+
+
+	void Update () {
+		Invoke(("Destruction"),timeBeforeDestruction);
+		StartCoroutine ("dontKillNow");
+
 	}
 
     void Destruction()
     {
+		if (sphereLight.intensity != 0) {
+			sphereLight.intensity -= 0.25f; 
+		} else {
 
-        Destroy(gameObject);
+			Destroy (gameObject);
+		}
     }
 }

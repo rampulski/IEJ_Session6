c352717fe0554a66b6ac4482279102bbf44261d9
Iej-11-PC\Game_11 <hevlaaskah@rust.re>
2017-09-29 12:27:27 +0200

    N'oubliez pas de tagger les 4 types d'orbs avec le tag "orbs", allez, la biz.

---
+++ IEJ_Session6/Assets/Scripts/SandStopsOrbs.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandStopsOrbs : MonoBehaviour {
+	public GameObject targetOrb;
+	public PhysicMaterial noBounceMat;
+
+
+	// Use this for initialization
+	void Start () {
+		targetOrb = GameObject.FindGameObjectWithTag ("orbs");
+		noBounceMat = new PhysicMaterial ();
+		noBounceMat.bounciness = 0;
+		noBounceMat.dynamicFriction = 50;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "orbs"){
+			other.material = noBounceMat;
+		}
+			
+	}
+
+}

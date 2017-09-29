c599d72503c4cf3e993038642a00296cdd36f525
Lenovo-PC\Sélom <akiel.arcosian@gmail.com>
2017-09-29 15:05:53 +0200

    Ajout de code intensité augmenté a chaque collision

--- IEJ_Session6/Assets/Scripts/Orbs.cs
+++ IEJ_Session6/Assets/Scripts/Orbs.cs
@@ -7,17 +7,19 @@ public class Orbs : MonoBehaviour {
 
 	public GameObject Sphere;
 	public Light sphereLight;
-
+    Collider OrbCollision;
+   
     public float timeBeforeDestruction;
 
 	void Start () {
 		
 
 		sphereLight = GetComponent<Light> ();
+        sphereLight.intensity.Equals(5); 
+        OrbCollision = gameObject.GetComponent(typeof(SphereCollider)) as Collider;
 
 
-
-	}
+    }
 	IEnumerator dontKillNow(){
 
 		yield return new WaitForSeconds (10);
@@ -26,12 +28,21 @@ public class Orbs : MonoBehaviour {
 
 	}
 
+    private void OnCollisionEnter(Collision OrbCollision)
+    {
+       
+            sphereLight.intensity -= 1.25f;
+           Debug.Log(sphereLight.intensity);
+        
+    }
 
-	void Update () {
-		Invoke(("Destruction"),timeBeforeDestruction);
-		StartCoroutine ("dontKillNow");
-
-	}
+    void Update() {
+        Invoke(("Destruction"), timeBeforeDestruction);
+        StartCoroutine("dontKillNow");
+        
+            
+        
+    }
 
     void Destruction()
     {

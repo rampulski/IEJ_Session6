11ef2c40fb113a120f4a0fab6df2e8821a1c89a3
IEJ-PC08\Game_04 <Unitysateur@hotmail.com>
2017-09-30 10:16:55 +0200

    MilkyWay Skybox

---
+++ IEJ_Session6/Assets/Scripts/RotateSky.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour {
+
+    public float RotateSpeed = 0.8f;
+
+	// Update is called once per frame
+	void Update () {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+	}
+}

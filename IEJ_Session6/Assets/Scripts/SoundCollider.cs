82d9dd3fc143f5f8c570e3f08d15b9aa3dd4f0e1
Lenovo-PC\Sélom <akiel.arcosian@gmail.com>
2017-10-03 15:53:19 +0200

    Script de son et son en question pour tester avec du son.

---
+++ IEJ_Session6/Assets/Scripts/SoundCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollider : MonoBehaviour {
+
+    public AudioSource SourceAudio;
+    public AudioClip hitOrb; 
+
+    private void OnCollisionEnter(Collision collision) // Teste la collision 
+    {
+
+        
+        SourceAudio.PlayOneShot(hitOrb, 1F); //Joue le son de la collision
+    }
+   
+}

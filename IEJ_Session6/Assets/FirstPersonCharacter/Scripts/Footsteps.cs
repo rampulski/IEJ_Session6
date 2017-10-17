d43ed26e1a8e032812cc0fb2a6cb9e0630e85127
Aure Lien <aurelien.iej2017@gmail.com>
2017-10-17 10:02:57 +0200

    Player Prefab (remplace RigidBodyFps)

---
+++ IEJ_Session6/Assets/FirstPersonCharacter/Scripts/Footsteps.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public AK.Wwise.Event FootStepEvent = null;
+
+    float footstepTimer = 0.0f;
+
+    Vector3 lastPos = new Vector3(0, 0, 0);
+
+    private void Awake()
+    {
+        lastPos = transform.position;
+    }
+
+    private void Update()
+    {
+        bool isMoving = (lastPos.x != transform.position.x || lastPos.z != transform.position.z);
+        lastPos = transform.position;
+
+        if (isMoving)
+        {
+            if (footstepTimer > 0.30f)
+            {
+                FootStepEvent.Post(gameObject);
+                footstepTimer = 0.0f;
+            }
+
+            footstepTimer += Time.deltaTime;
+        }
+    }
+}

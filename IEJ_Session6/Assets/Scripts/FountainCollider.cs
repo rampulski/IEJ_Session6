82409c9cf5a8283469aba993d5657315305d6610
IEJ-PC07\Game_03 <lecl.gg@gmail.com>
2017-10-20 15:29:54 +0200

    Scene parc + lac + prefab fontaine + script fontaine

---
+++ IEJ_Session6/Assets/Scripts/FountainCollider.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FountainCollider : MonoBehaviour {
+    public ParticleSystem ps;
+    public bool moduleEnabled;
+    public GameObject fountain;
+    
+    void Start()
+    {
+        StopFountain();
+        fountain = GameObject.FindGameObjectWithTag("fountain");
+        ps = fountain.GetComponentInChildren<ParticleSystem>();
+
+    }
+    void Update()
+    {
+        var emission = ps.emission;
+        emission.enabled = moduleEnabled;
+    } 
+    private void OnTriggerEnter(Collider col) 
+    {
+        StartCoroutine(LaunchStop());
+    }
+    IEnumerator LaunchStop()
+    {
+        PlayFountain();
+        yield return new WaitForSeconds(10);
+        StopFountain();
+    }
+
+
+    private void PlayFountain()
+    {
+        moduleEnabled = true;
+    }
+    private void StopFountain()
+    {
+        moduleEnabled = false;
+    }
+
+
+
+
+}

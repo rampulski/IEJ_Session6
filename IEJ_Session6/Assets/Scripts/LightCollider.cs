21dd48d1db041d7234830d97c53be59097c0808e
Lenovo-PC\Sélom <akiel.arcosian@gmail.com>
2017-10-20 14:55:06 +0200

    Mise à jour du script de lampe

--- IEJ_Session6/Assets/Scripts/LightCollider.cs
+++ IEJ_Session6/Assets/Scripts/LightCollider.cs
@@ -5,35 +5,40 @@ using UnityEngine;
 public class LightCollider : MonoBehaviour {
     
     public Light Lampe;
-    
     public Renderer rend;
   
  void Start() {
 
-       
+        
         Lampe.intensity = 0f;
         rend.material.SetColor("_MainColor", Color.black);
-
-
-    }
+}
     void Update()
     {
         
     }
     private void OnTriggerEnter(Collider col) // Teste la collision 
     {
-        Lampe.intensity = 0.8f;
-        
+
+        StartCoroutine("DontKillMe");
+
+
+
+
+    }
+
+    IEnumerator DontKillMe()
+    {
+       Lampe.intensity = 1.5f;
         rend.material.SetColor("_MainColor", Color.white);
-        
-        
+        yield return new WaitForSeconds(10f);
+        Lampe.intensity = 0f;
+        rend.material.SetColor("_MainColor", Color.black);
 
     }
 
 
 
-
-   
 
 }
         

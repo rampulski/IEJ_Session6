e8859bee5e3e8a06dc134553339a8a047d546b0d
Lenovo-PC\Sélom <akiel.arcosian@gmail.com>
2017-10-20 12:21:40 +0200

    Starter kit lampe qui s'allument quand on lance une orbe vers elle

---
+++ IEJ_Session6/Assets/Scripts/LightCollider.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCollider : MonoBehaviour {
+    
+    public Light Lampe;
+    
+    public Renderer rend;
+  
+ void Start() {
+
+       
+        Lampe.intensity = 0f;
+        rend.material.SetColor("_MainColor", Color.black);
+
+
+    }
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider col) // Teste la collision 
+    {
+        Lampe.intensity = 0.8f;
+        
+        rend.material.SetColor("_MainColor", Color.white);
+        
+        
+
+    }
+
+
+
+
+   
+
+}
+        
+        
+
+	
+	
+
